03ccfcdb0985b1e3fac56fad6ebbde7e9908c808
patrickdevy <77765549+patrickdevy@users.noreply.github.com>
2023-04-02 19:47:22 -0400

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,11 @@
+<?php
+
+  $to = "dantehtop5@gmail.com.com";
+
+  $headers = "From: $email_from \r\n";
+
+  $headers .= "Reply-To: $visitor_email \r\n";
+
+  mail($to,$email_subject,$email_body,$headers);
+
+ ?>
